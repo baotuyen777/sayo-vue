cc4b86a5b927fcc6797d61db17bc198a7befd7e4
tuyenbv <baotuyen111@gmail.com>
2023-07-15 16:42:46 +0700

    upload post avatar

--- resources/js/src/components/layouts/TheHeader.vue
+++ resources/js/src/components/layouts/TheHeader.vue
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row text-white" style="background-color: green; padding: 1rem">
             <div class="col-1 d-flex d-sm-none align-items-center justify-content-center">
-                <span @click="showDrawer"><font-awesome-icon :icon="['fas', 'align-justify']" /></span>
+                <span @click="showDrawer"><i class="fa-solid fa-align-justify"></i></span>
             </div>
 
             <div class="col-10 col-sm-9 d-flex align-items-center justify-content-center justify-content-sm-start">
@@ -15,7 +15,7 @@
             </div>
 
             <div class="col-1 d-flex d-sm-none align-items-center justify-content-center">
-                <span @click="showDrawerUser"><font-awesome-icon :icon="['fas', 'user']" /></span>
+                <span @click="showDrawerUser"><i class="fa-solid fa-user"></i></span>
             </div>
         </div>
     </div>
