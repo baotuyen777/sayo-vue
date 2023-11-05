f7845625f4df3ec544a4cd736d3283c3a78ef148
tuyenbv <baotuyen111@gmail.com>
2023-11-05 23:44:11 +0700

    refactor post

--- database/migrations/2023_05_14_235508_alter_users_table.php
+++ database/migrations/2023_05_14_235508_alter_users_table.php
@@ -21,6 +21,7 @@ return new class extends Migration {
             $table->date('birthday')->nullable();
             $table->tinyInteger('gender')->nullable();
             $table->string('address')->unique()->nullable();
+            $table->string('google_id')->nullable();
 
             $table->unsignedBigInteger('avatar_id')->nullable();
             $table->foreign('avatar_id')->references('id')->on('files');
