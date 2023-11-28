cd30a2c11d82678fcc1113aec6c103f1191a1a22
binhnl <binhnluu@gmail.com>
2023-11-28 12:44:57 +0700

    fix value avg_rate in product

--- resources/views/component/review/index.blade.php
+++ resources/views/component/review/index.blade.php
@@ -65,19 +65,19 @@
                                         <label for="rate-1" style="--i:1"><i class="fa fa-star"></i></label>
                                         <input type="radio" name="rating" id="rate-1" value="1">
                                         <label for="rate-2" style="--i:2"><i class="fa fa-star"></i></label>
-                                        <input type="radio" name="rating" id="rate-2" value="2" checked>
+                                        <input type="radio" name="rating" id="rate-2" value="2">
                                         <label for="rate-3" style="--i:3"><i class="fa fa-star"></i></label>
                                         <input type="radio" name="rating" id="rate-3" value="3">
                                         <label for="rate-4" style="--i:4"><i class="fa fa-star"></i></label>
                                         <input type="radio" name="rating" id="rate-4" value="4">
                                         <label for="rate-5" style="--i:5"><i class="fa fa-star"></i></label>
-                                        <input type="radio" name="rating" id="rate-5" value="5">
+                                        <input type="radio" name="rating" id="rate-5" value="5" checked>
                                     </span>
                                 </p>
                             </td>
                             <td colspan="3">
                                 <textarea name="content" rows="5" placeholder="Đánh giá ..."></textarea>
-                                @include('component.form.uploadFiles')
+                                @include('component.form.uploadFiles', ['obj' => null])
                                 <button type="submit" class="btn btn-yellow">Đánh giá</button>
                             </td>
                         </tr>
